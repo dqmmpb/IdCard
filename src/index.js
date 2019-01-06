82bb6b500e5b8bf45f1ef05c555d66a12ec36c9f
dqmmpb <dqmmpb@163.com>
2019-01-06 16:29:44 +0800

    删除pad相关函数

--- src/index.js
+++ src/index.js
@@ -68,70 +68,6 @@ function endNum(idCard) {
 }
 
 /**
- * 左补位
- * @param val 待补位字符串
- * @param num 补位后字符串长度
- * @param pad 补位字符，默认使用'0'进行补位；支持多字符补位
- *            例如：
- *            1. leftPad('123', 10, 'A') => 'AAAAAAA123'
- * @returns {string}
- */
-function leftPad(val, num, pad) {
-  const str = String(val);
-  const padStr = (!nullOrUndefined(pad) && String(pad).length >= 0)
-    ? String(pad)
-    : '0';
-  let padding = '';
-  let diff = num - str.length;
-
-  if (diff > 0) {
-    const padLen = padStr.length;
-    do {
-      if (diff < padLen) {
-        padding = padStr.slice(padStr.length - diff) + padding;
-      } else {
-        padding = padStr + padding;
-      }
-      diff -= padLen;
-    } while (diff > 0 && padLen > 0);
-  }
-
-  return padding + str;
-}
-
-/**
- * 右补位
- * @param val 待补位字符串
- * @param num 补位后字符串长度
- * @param pad 补位字符，默认使用'0'进行补位；支持多字符补位
- *            例如：
- *            1. rightPad('123', 10, 'A') => '123AAAAAAA'
- * @returns {string}
- */
-function rightPad(val, num, pad) {
-  const str = String(val);
-  const padStr = (!nullOrUndefined(pad) && String(pad).length >= 0)
-    ? String(pad)
-    : '0';
-  let padding = '';
-  let diff = num - str.length;
-
-  if (diff > 0) {
-    const padLen = padStr.length;
-    do {
-      if (diff < padLen) {
-        padding += padStr.slice(0, diff);
-      } else {
-        padding += padStr;
-      }
-      diff -= padLen;
-    } while (diff > 0 && padLen > 0);
-  }
-
-  return str + padding;
-}
-
-/**
  * 农历转换
  * @param birthday
  * @returns {string}
@@ -139,7 +75,7 @@ function rightPad(val, num, pad) {
 function solarToLunar(birthday) {
   try {
     const lunar = chineseLunar.solarToLunar(new Date(birthday));
-    return `${lunar.year}/${leftPad(lunar.month, 2, '0')}/${leftPad(lunar.day, 2, '0')}`;
+    return `${lunar.year}/${String(lunar.month).padStart(2, '0')}/${String(lunar.day).padStart(2, '0')}`;
   } catch (err) {
     throw err;
   }
@@ -280,6 +216,4 @@ module.exports = {
       address: address(idCard),
     };
   },
-  leftPad,
-  rightPad,
 };
